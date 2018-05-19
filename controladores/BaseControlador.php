5191fa7995917f1dd48727b4706bfda5525d82a6
Violinistae <ssbbemims@gmail.com>
2018-05-18 21:04:31 -0500

    UsuarioControlador modificado para uso con WS

--- controladores/BaseControlador.php
+++ controladores/BaseControlador.php
@@ -3,10 +3,11 @@
         protected $wsdl = "'http://ip_equipo:8080/ws_test/server.php?wsdl';";
         protected $soapClient;
 
+        protected $availableWSActions;
 
         public function __construct() {
             $this->soapClient = new SoapClient($wsdl);
-            
+            $this->availableWSActions = $this->soapClient->_getFunctions();
         }
     }
     
--- controladores/UsuarioControlador.php
+++ controladores/UsuarioControlador.php
@@ -1,19 +1,52 @@
 <?php
 
-    Class UsuarioControlador extends DBConexion {
+    Class UsuarioControlador extends BaseControlador {
 
         public $result = 1;
-        private $tabla = "usuarios";
+        //private $tabla = "usuarios";
         public $key = '';
+        
+        /*
         private $fields = array(
             "user" => "username",
             "pwd"  => "password",
             "hash" => "hash"
         );
+        */
 
         public function __construct(){}
 
-        public function Ingresar(){
+        public function Ingresar () {
+            if($_SERVER["REQUEST_METHOD"]=="POST" && isset($_POST["usuario"]) && isset($_POST["password"])):
+                
+                if(empty($_POST["usuario"]) && empty($_POST["password"])):
+                    $this->result = 2;
+                    return;
+                endif;
+
+                $username = trim($_POST["usuario"]);
+                $password = hash("sha256", $_POST["password"]);
+
+                $loginRes = $this->soapClient->__soapCall('Ingresar', array('username' => $username, 'password' => $password));
+
+                if ($loginRes > 0): 
+                    $key = hash("sha256",(string)mt_rand(10, 1000));
+                    $this->soapClient->__soapCall('SetHash', array('key' => $key));
+
+                    $_SESSION["usuario"] = $username;
+                    $_SESSION["hash"] = $key;
+                    header("Location: producto.php?c=Producto&a=Lista");
+                else:
+                    $this->result = 3;
+                    return false;
+                endif;
+
+                
+            endif;
+        }
+
+        /*
+        public function Ingresar() {
 
             if($_SERVER["REQUEST_METHOD"]=="POST" && isset($_POST["usuario"]) && isset($_POST["password"])):
                 
@@ -57,7 +90,37 @@
             endif;    
 
         }   
+        */
 
+        public function Registrar () {
+            if($_SERVER["REQUEST_METHOD"]=="POST" && isset($_POST["usuario"]) && isset($_POST["password"])):
+
+                if(empty($_POST["usuario"]) && empty($_POST["password"])):
+                    $this->result = 2;
+                    return;
+                endif;
+
+                $username = trim($_POST["usuario"]);
+                $password = hash("sha256", $_POST["password"]);
+                $key = hash("sha256",(string)mt_rand(10, 1000));
+
+                $registerUserRes = $this->soapClient->__soapCall('SelectUserByUsername', array('username' => $username, 'password' => $password, 'key'=> $key));
+
+                if ($registerUserRes == 3) {        //Ya existe ese nombre de usuario
+                    $this->result = 3;
+                    return;
+                } else if ($registerUserRes == 1) {
+
+                    $_SESSION["usuario"] = $username;
+                    $_SESSION["hash"] = $key;                        
+                    header("Location: producto.php");
+
+                }                        
+
+            endif;
+        }
+
+        /*
         public function Registrar(){
 
             if($_SERVER["REQUEST_METHOD"]=="POST" && isset($_POST["usuario"]) && isset($_POST["password"])):
@@ -115,12 +178,30 @@
             endif;    
 
         }
+        */
 
         public function Logout(){
             session_destroy();
             header("Location: usuario.php?c=Usuario&a=Ingresar");
         }
 
+        public function Check () {
+            if(isset($_SESSION["hash"])):
+                $key = $_SESSION["hash"];
+                $resCheckHash = $this->soapClient->__soapCall('CheckHash', array('key' => $key));
+
+                if ($resCheckHash == 1) {
+                    return false;
+                } else {
+                    session_destroy();
+                    return true;
+                }
+            else:
+                return true;
+            endif;            
+        }
+
+        /*
         public function Check(){
 
             if(isset($_SESSION["hash"])):
@@ -148,6 +229,7 @@
             endif;
 
         }
+        */
 
     }
 
